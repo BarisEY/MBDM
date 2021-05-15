d919e65be042d0866552c914d7773fe644949d9f
barisey <Baris@yakali.net>
2021-05-15 13:32:12 +0200

    Finished a bit more
    Properly finished assignment 6 with a small explanation at the end.

--- Baris-Assignments/week3-4/assignment 6.py
+++ Baris-Assignments/week3-4/assignment 6.py
@@ -12,6 +12,7 @@ from ema_workbench.analysis.scenario_discovery_util import RuleInductionType
 from ema_workbench.em_framework.salib_samplers import get_SALib_problem
 from SALib.analyze import sobol
 import statsmodels.api as sm
+import seaborn as sns
 import statsmodels.formula.api as smf
 from ema_workbench import (RealParameter, ScalarOutcome, Constant, Model)
 
@@ -45,18 +46,11 @@ def pred_prey(prey_birth_rate=0.025, predation_rate=0.0015, predator_efficiency=
             'predators': predators,
             'prey': prey}
 
-
-def analyze_results_final_value(results, variable):
+def analyze_results(results, variable, analyze_array):
     _, outcomes = results
     problem = get_SALib_problem(model.uncertainties)
     y = outcomes[variable]
-    final_values = np.zeros(shape=len(y))
-    for i in range(len(y)):
-        a = y[i]
-        b = a[-1][-1]
-        final_values[i] = b
-    # y[:] = y[:][-1]
-    sobol_indices = sobol.analyze(problem, final_values)
+    sobol_indices = sobol.analyze(problem, analyze_array)
     sobol_stats = {key: sobol_indices[key] for key in ['ST', 'ST_conf', 'S1',
                                                        'S1_conf']}
     sobol_stats = pd.DataFrame(sobol_stats, index=problem['names'])
@@ -68,94 +62,66 @@ def analyze_results_final_value(results, variable):
 
     return sobol_stats, s2, s2_conf
 
-def analyze_results_mean_value(results, variable):
-    _, outcomes = results
-    problem = get_SALib_problem(model.uncertainties)
-    y = outcomes[variable]
-    final_values = np.zeros(shape=len(y))
-    for i in range(len(y)):
-        a = y[i]
-        b = a[-1]
-        c = b.mean()
-        final_values[i] = c
-    # y[:] = y[:][-1]
-    sobol_indices = sobol.analyze(problem, final_values)
-    sobol_stats = {key: sobol_indices[key] for key in ['ST', 'ST_conf', 'S1',
-                                                       'S1_conf']}
-    sobol_stats = pd.DataFrame(sobol_stats, index=problem['names'])
-    sobol_stats.sort_values(by='ST', ascending=False)
-    s2 = pd.DataFrame(sobol_indices['S2'], index=problem['names'],
-                      columns=problem['names'])
-    s2_conf = pd.DataFrame(sobol_indices['S2_conf'], index=problem['names'],
-                           columns=problem['names'])
 
-    return sobol_stats, s2, s2_conf
+if __name__ == '__main__':
+    ema_logging.log_to_stderr(ema_logging.INFO)
+    #Creating a model instance
+    model = Model('predator', function=pred_prey)
 
-def analyze_results_standard_dev_value(results, variable):
-    _, outcomes = results
-    problem = get_SALib_problem(model.uncertainties)
-    y = outcomes[variable]
-    final_values = np.zeros(shape=len(y))
-    for i in range(len(y)):
-        a = y[i]
-        b = a[-1]
-        c = b.std()
-        final_values[i] = c
-    # y[:] = y[:][-1]
-    sobol_indices = sobol.analyze(problem, final_values)
-    sobol_stats = {key: sobol_indices[key] for key in ['ST', 'ST_conf', 'S1',
-                                                       'S1_conf']}
-    sobol_stats = pd.DataFrame(sobol_stats, index=problem['names'])
-    sobol_stats.sort_values(by='ST', ascending=False)
-    s2 = pd.DataFrame(sobol_indices['S2'], index=problem['names'],
-                      columns=problem['names'])
-    s2_conf = pd.DataFrame(sobol_indices['S2_conf'], index=problem['names'],
-                           columns=problem['names'])
+    #specify uncertainties
+    model.uncertainties = [RealParameter('prey_birth_rate', 0.015, 0.035),
+                           RealParameter('predation_rate', 0.0005, 0.003),
+                           RealParameter('predator_efficiency', 0.001, 0.004),
+                           RealParameter('predator_loss_rate', 0.04, 0.08)]
 
-    return sobol_stats, s2, s2_conf
+    model.outcomes = [TimeSeriesOutcome('TIME'),
+                      TimeSeriesOutcome('predators'),
+                      TimeSeriesOutcome('prey')]
 
-# if __name__ == '__main__':
-ema_logging.log_to_stderr(ema_logging.INFO)
-#Creating a model instance
-model = Model('predator', function=pred_prey)
+    model.constants = [Constant('initial_prey', 50),
+                       Constant('initial_predators', 20),
+                       Constant('dt', 0.25),
+                       Constant('final_time', 365),
+                       Constant('reps', 1)]
 
-#specify uncertainties
-model.uncertainties = [RealParameter('prey_birth_rate', 0.015, 0.035),
-                       RealParameter('predation_rate', 0.0005, 0.003),
-                       RealParameter('predator_efficiency', 0.001, 0.004),
-                       RealParameter('predator_loss_rate', 0.04, 0.08)]
+    n_scenarios_1 = 50
+    n_scenarios_2 = 250
+    n_scenarios_3 = 1000
 
-model.outcomes = [TimeSeriesOutcome('TIME'),
-                  TimeSeriesOutcome('predators'),
-                  TimeSeriesOutcome('prey')]
-
-model.constants = [Constant('initial_prey', 50),
-                   Constant('initial_predators', 20),
-                   Constant('dt', 0.25),
-                   Constant('final_time', 365),
-                   Constant('reps', 1)]
-
-n_scenarios_1 = 50
-n_scenarios_2 = 250
-n_scenarios_3 = 1000
-
-with ema_workbench.SequentialEvaluator(model) as evaluator:
-    results = evaluator.perform_experiments(n_scenarios_1, uncertainty_sampling=SOBOL)
+    with ema_workbench.MultiprocessingEvaluator(model) as evaluator:
+        results = evaluator.perform_experiments(n_scenarios_1, uncertainty_sampling=SOBOL)
 
 # with ema_workbench.MultiprocessingEvaluator(model) as evaluator:
 #     results = evaluator.perform_experiments(n_scenarios_1, uncertainty_sampling=SOBOL)
 
-sobol_stats, s2, s2_conf = analyze_results_final_value(results, 'prey')
-print(sobol_stats)
-print(s2)
-print(s2_conf)
-
-sobol_stats, s2, s2_conf = analyze_results_mean_value(results, 'prey')
-print(sobol_stats)
-print(s2)
-print(s2_conf)
-
-sobol_stats, s2, s2_conf = analyze_results_standard_dev_value(results, 'prey')
-print(sobol_stats)
-print(s2)
-print(s2_conf)
+    x, outcomes = results
+    y = outcomes['prey']
+    last_values = np.zeros(shape=len(y))
+    mean_values = np.zeros(shape=len(y))
+    standard_dev_values = np.zeros(shape=len(y))
+    for i in range(len(y)):
+        last_values[i] = y[i][-1][-1]
+        mean_values[i] = y[i][-1].mean()
+        standard_dev_values[i] = y[i][-1].std()
+    # y[:] = y[:][-1]
+    x = x.drop(['model', 'policy'], axis=1)
+    y = np.max(outcomes['prey'], axis=1)# sobol_stats, s2, s2_conf = analyze_results(results, 'prey', last_values)
+    # print(sobol_stats)
+    # print(s2)
+    all_scores = []# print(s2_conf)
+    # Change this to 100, although it increases the time by a lot
+    for i in range(10):#
+        print(f"Running {i}")
+        indices = np.random.choice(np.arange(0, x.shape[0]), size=x.shape[0])# sobol_stats, s2, s2_conf = analyze_results(results, 'prey', mean_values)
+        selected_x = x.iloc[indices, :]# print(sobol_stats)
+        selected_y = y[indices]# print(s2)
+        # print(s2_conf)
+        scores = feature_scoring.get_ex_feature_scores(selected_x,
+                                                       selected_y,
+                                                       max_features=0.6,
+                                                       mode=RuleInductionType.REGRESSION)[0]# sobol_stats, s2, s2_conf = analyze_results(results, 'prey', standard_dev_values)
+        all_scores.append(scores)# print(sobol_stats)
+    all_scores = pd.concat(all_scores, axis=1, sort=False)# print(s2)
+    # print(s2_conf)
+    sns.boxplot(data=all_scores.T)
+    plt.show()
